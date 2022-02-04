c20199bd4b7f2257d7af972407a12828f1009360
tchase44 <tchase4544@gmail.com>
2022-02-04 09:51:32 -0500

    proxy

--- vue/src/axios.js
+++ vue/src/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 // import store from './store'?
 // axios.defaults.baseURL = 'https://api.example.com';
-axios.defaults.baseURL = 'http://localhost:3000'; // remove once proxy os setup
+axios.defaults.baseURL = 'https://localhost:3000'; // remove once proxy os setup
 axios.defaults.headers.common['Content-Type'] = 'application/json';
 axios.defaults.headers.common['Authorization'] = `Bearer ${sessionStorage.getItem("auth_token")}`;
 
