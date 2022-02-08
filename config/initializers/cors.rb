976b04e061471c78f0bc2cb2da10858fdee0ce8a
tchase44 <tchase4544@gmail.com>
2022-02-08 14:44:30 -0500

    cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,8 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'# 'http://localhost:8080'
+    Rails.env.production? origins 'https://tims-recipe-book.herokuapp.com'
+    Rails.env.development? origins '*'# 'http://localhost:8080'
 
     resource '*',
       headers: :any,
